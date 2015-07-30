64d51556ba656fb7d4c30f7c9c7d2fee66efe8c2
Iñaki Plessmann <inaki@hecticus.com>
2015-07-30 16:09:15 -0430

    vistas de country, language, timezone y devices

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,6 +7,9 @@ import views.html.*;
 
 public class Application extends Controller {
 
+    public static final String USER_ROLE = "user";
+    public static final String ADMIN_ROLE = "admin";
+
     public static Result alive(){
         return ok("alive");
     }
